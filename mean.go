b128bedef5201a9353936fbf9c6d3cf7f4b03a11
Aaron Burk <aburk@veracode.com>
2022-10-19 15:09:55 -0400

    declare and assign vars at once using :=

--- mean.go
+++ mean.go
@@ -6,11 +6,8 @@ import (
 )
 
 func main() {
-	var x float64
-	var y float64
-
-	x = 1.0
-	y = 2.0
+	x := 1.0
+	y := 2.0
 
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
