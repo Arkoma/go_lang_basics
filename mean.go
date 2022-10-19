a6ec380266192be5d21c4f01e779b2e05bc8f6de
Aaron Burk <aburk@veracode.com>
2022-10-19 15:06:52 -0400

    mean with floats

--- mean.go
+++ mean.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	var x int
-	var y int
+	var x float64
+	var y float64
 
-	x = 1
-	y = 2
+	x = 1.0
+	y = 2.0
 
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
