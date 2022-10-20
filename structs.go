5e298502d129ccdf16eda5e962d260f91f1d316f
Aaron Burk <aburk@veracode.com>
2022-10-20 16:23:51 -0400

    add methods to budget struct

--- structs.go
+++ structs.go
@@ -1,4 +1,4 @@
-// Errors
+// Budget struct with methods
 package main
 
 import (
@@ -12,22 +12,19 @@ type Budget struct {
 	Expires    time.Time
 }
 
-// all fields that start with a capital are exported (like public in Java)
-// any fields that start with a lower case are not exported (or private)
+func (b Budget) TimeLeft() time.Duration {
+	return b.Expires.Sub(time.Now().UTC())
+}
+
+func (b *Budget) Update(sum float64) {
+	b.Balance += sum
+}
 
 func main() {
-	b1 := Budget{"Kittens", 22.3, time.Now().Add(7 * 24 * time.Hour)}
-	fmt.Println(b1)
-	fmt.Printf("%#v\n", b1)
-	fmt.Println(b1.CampaignID)
+	b := Budget{"Kittens", 22.3, time.Now().Add(7 * 24 * time.Hour)}
+	fmt.Println(b.TimeLeft())
 
-	b2 := Budget{
-		Balance:    19.3,
-		CampaignID: "puppies",
-	}
-	fmt.Printf("%#v\n", b2) // Expiration returns its zero value
-
-	var b3 Budget
-	fmt.Printf("%#v\n", b3) // all values return their zero value
+	b.Update(10.5)
+	fmt.Println(b.Balance)
 
 }
