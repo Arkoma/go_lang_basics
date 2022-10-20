5e57cf0dde082ba1c5dc0c616e2ddcb4546202fa
Aaron Burk <aburk@veracode.com>
2022-10-20 10:36:58 -0400

    sices maximal value challenge ... better solution

--- slices.go
+++ slices.go
@@ -7,12 +7,15 @@ import (
 
 func main() {
 	nums := []int{16, 8, 42, 23, 15}
-	maxnum := 0
-	for _, num := range nums {
+	maxnum := nums[0]
+
+	for _, num := range nums[1:] {
+
 		if num > maxnum {
 			maxnum = num
 		}
 
 	}
+
 	fmt.Println(maxnum)
 }
