90e5fb32809fd36aebe6f0612db4ed9c1f0e4105
Aaron Burk <aburk@veracode.com>
2022-10-21 16:09:09 -0400

    using routines and channels to make async calls

---
+++ sites.go
@@ -0,0 +1,38 @@
+// Get content-type from sites using channels and routines
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func returnType(url string, out chan string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		out <- fmt.Sprintf("%s -> error: %s", url, err)
+		return
+	}
+	defer resp.Body.Close()
+	ctype := resp.Header.Get("content-type")
+	out <- fmt.Sprintf("%s -> %s", url, ctype)
+}
+
+func main() {
+	urls := []string{
+		"https://golang.org",
+		"https://api.github.com",
+		"https://httpbin.org/xml",
+	}
+
+	// Create response channel
+	ch := make(chan string)
+	for _, url := range urls {
+		go returnType(url, ch)
+	}
+
+	for range urls {
+		out := <-ch // this line will block until we get a value from the channel
+		fmt.Println(out)
+	}
+
+}
