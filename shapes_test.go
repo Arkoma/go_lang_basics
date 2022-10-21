cced8334a9c4aa24e2adbe47d61a3a31a9bb1279
Aaron Burk <aburk@veracode.com>
2022-10-21 10:58:05 -0400

    refactor to use subtests

--- shapes_test.go
+++ shapes_test.go
@@ -5,22 +5,24 @@ import (
 	"testing"
 )
 
-func TestSquareAreaWithLengthOf7(t *testing.T) {
-	s := Square{7}
-	got := s.Area()
-	want := 49.0
-	if got != want {
-		t.Errorf("got %f want %f", got, want)
-	}
-}
+func TestSquare(t *testing.T) {
+	t.Run("test square with length of 7", func(t *testing.T) {
+		s := Square{7}
+		got := s.Area()
+		want := 49.0
+		if got != want {
+			t.Errorf("got %f want %f", got, want)
+		}
+	})
 
-func TestSquareAreaWithLengthOf10(t *testing.T) {
-	s := Square{10}
-	got := s.Area()
-	want := 100.0
-	if got != want {
-		t.Errorf("got %f want %f", got, want)
-	}
+	t.Run("test square with length of 10", func(t *testing.T) {
+		s := Square{10}
+		got := s.Area()
+		want := 100.0
+		if got != want {
+			t.Errorf("got %f want %f", got, want)
+		}
+	})
 }
 
 func TestCircleAreaWithRadiusOf2(t *testing.T) {
