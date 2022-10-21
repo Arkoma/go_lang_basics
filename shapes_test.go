ed8534e1730a8301b0e5b16b7a7f84229e65ce1f
Aaron Burk <aburk@veracode.com>
2022-10-21 11:12:10 -0400

    add test helper method

--- shapes_test.go
+++ shapes_test.go
@@ -10,19 +10,22 @@ func TestSquare(t *testing.T) {
 		s := Square{7}
 		got := s.Area()
 		want := 49.0
-		if got != want {
-			t.Errorf("got %f want %f", got, want)
-		}
+		assertFloatsEqual(t, got, want)
 	})
 
 	t.Run("test square with length of 10", func(t *testing.T) {
 		s := Square{10}
 		got := s.Area()
 		want := 100.0
-		if got != want {
-			t.Errorf("got %f want %f", got, want)
-		}
+		assertFloatsEqual(t, got, want)
 	})
+}
+
+func assertFloatsEqual(t testing.TB, got, want float64) {
+	t.Helper() // needed so that test will report line number from function call instead of line number in the helper method
+	if got != want {
+		t.Errorf("got %f want %f", got, want)
+	}
 }
 
 func TestCircleAreaWithRadiusOf2(t *testing.T) {
